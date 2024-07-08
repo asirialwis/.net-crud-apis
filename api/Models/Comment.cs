0f19e688a46df928f0c7e407af5b64f75ad3b5df
asirialwis <asirichathurika900@gmail.com>
2024-07-08 09:08:23 +0530

    project init & stock,comment models add

---
+++ api/Models/Comment.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        
+        
+    }
+}
