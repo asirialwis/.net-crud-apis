3b80ab0763f392bb8cef92e66ac4a93821a9624c
asirialwis <asirichathurika900@gmail.com>
2024-07-13 17:35:09 +0530

    connection string added

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -9,7 +9,17 @@ namespace api.Models
 {
     public class Comment
     {
-        
+        public int Id { get; set; }
+        public String Title { get; set; } = String.Empty;
+
+        public String Content { get; set; } = String.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+         
+         public int? StockId { get; set; }
+        //Navigation property
+        public Stock? stock { get; set; }
+
         
     }
 }
